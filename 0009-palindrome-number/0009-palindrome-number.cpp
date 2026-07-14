631aa2fe8acb6a2a515aaf4754a3e40ef9f38255
sanyogitamishra2645-svg <sanyogitamishra2645@gmail.com>
2026-07-15 05:20:23 +0530

    Time: 0 ms (100%), Space: 10.8 MB (13.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+       string s=to_string(x);
+        int n=s.size();
+       for(int i=0 ; i<n/2 ; i++){
+            if(s[i]==s[n-1-i]){
+                continue ;
+            }
+            else{
+                return false ;
+            }
+       }
+       return true ;
+    }
+};
