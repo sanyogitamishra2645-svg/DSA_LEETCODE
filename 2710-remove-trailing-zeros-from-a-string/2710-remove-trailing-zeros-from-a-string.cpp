e346b515312ee7264be5d1c9c670217bc85949e2
sanyogitamishra2645-svg <sanyogitamishra2645@gmail.com>
2026-07-28 03:26:54 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.96%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string removeTrailingZeros(string num) {
+        if(num.size()==0){
+            return num;
+        }
         while(num.back()=='0'){
             num.pop_back();
         }
