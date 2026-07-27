f5c58764b07dcdc6298ff48d5645b28bc8f55795
sanyogitamishra2645-svg <sanyogitamishra2645@gmail.com>
2026-07-28 03:24:48 +0530

    Time: 4 ms (16.78%), Space: 11.6 MB (55.53%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     string removeTrailingZeros(string num) {
-        while(num.size()>0){
-            if(num[num.size()-1]== '0'){
-                num.pop_back();
-            }
-            else{
-                return num;
-            }
+        while(num.back()=='0'){
+            num.pop_back();
         }
         return num;
     }
