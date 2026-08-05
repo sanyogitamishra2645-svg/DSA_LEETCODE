8450ad91c1623966d8280cb6f36cf65c93fd4db6
sanyogitamishra2645-svg <sanyogitamishra2645@gmail.com>
2026-08-05 10:18:39 +0530

    Time: 0 ms (100%), Space: 11.7 MB (85.15%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left=0;
+        for(int right=0 ; right<nums.size(); right++){
+            if(nums[right] != val){
+                nums[left]=nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+};
